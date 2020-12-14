82b86e7cf2295c40836199f265efb4e095a48d43
= <uzzini@naver.com>
2020-12-14 15:21:03 +0900

    first

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: any) {
+    console.log(movieData);
+    return movieData;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    // return `This will patch a movie with the id: ${movieId}`;
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
